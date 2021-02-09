a347be4ac134df6f40ffc43c5df789ef661d4bc1
ron93 <ronmkare1@gmail.com>
2021-02-10 00:29:38 +0300

    merge

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# my-portfolio
+# Covid data analysis
--- js/app.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('.home-nav').on('click', function() {
-        $(".home-nav").hide();
-        
-    });
- 
-});
